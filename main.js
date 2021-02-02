e10b65b7beb91234c5df01f2cbb721d8584f20d7
Jorge Ramírez <72385150+JorgeRamirezDEV@users.noreply.github.com>
2021-02-02 20:24:04 +0100

    .

---
+++ main.js
@@ -0,0 +1,44 @@
+window.onload = function (){
+
+}
+
+var app= new Vue({
+    el:'#app',
+    data:{
+        data:null,
+        es:true
+},
+
+
+mounted(){
+        axios.get('texto.json')
+        .then((response)=> {
+                this.data=response.data
+        })
+        .catch(function (error){
+                console.log("Error al cargar el JSON" +error);
+        });
+},
+methods:{
+    cambiarIdioma: function () {
+        this.es=!this.es
+    }
+
+},
+
+
+computed:{
+    obtenerTexto: function () {
+        if (this.data === null) {
+                return "";
+        }
+        if (this.es) {
+                return this.data.es;
+        } else {
+                return this.data.en;
+        }
+    },
+        
+}
+
+})
